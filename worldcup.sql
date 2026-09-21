8a790c6b34eb67fde251729aa68bf66fd0d6b8a5
Omarmuslh <muslhomar32@gmail.com>
2026-09-21 11:11:45 -0400

    Add all required project files

--- worldcup.sql
+++ worldcup.sql
@@ -16,29 +16,17 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE worldcup;
---
--- Name: worldcup; Type: DATABASE; Schema: -; Owner: freecodecamp
---
-
-CREATE DATABASE worldcup WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
-
-
-ALTER DATABASE worldcup OWNER TO freecodecamp;
-
-\connect worldcup
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
+ALTER TABLE ONLY public.games DROP CONSTRAINT games_winner_id_fkey;
+ALTER TABLE ONLY public.games DROP CONSTRAINT games_opponent_id_fkey;
+ALTER TABLE ONLY public.teams DROP CONSTRAINT teams_pkey;
+ALTER TABLE ONLY public.teams DROP CONSTRAINT teams_name_key;
+ALTER TABLE ONLY public.games DROP CONSTRAINT games_pkey;
+ALTER TABLE public.teams ALTER COLUMN team_id DROP DEFAULT;
+ALTER TABLE public.games ALTER COLUMN game_id DROP DEFAULT;
+DROP SEQUENCE public.teams_team_id_seq;
+DROP TABLE public.teams;
+DROP SEQUENCE public.games_game_id_seq;
+DROP TABLE public.games;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -134,68 +122,72 @@ ALTER TABLE ONLY public.teams ALTER COLUMN team_id SET DEFAULT nextval('public.t
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.games VALUES (33, 2018, 'Final', 25, 26, 4, 2);
-INSERT INTO public.games VALUES (34, 2018, 'Third Place', 27, 28, 2, 0);
-INSERT INTO public.games VALUES (35, 2018, 'Semi-Final', 26, 28, 2, 1);
-INSERT INTO public.games VALUES (36, 2018, 'Semi-Final', 25, 27, 1, 0);
-INSERT INTO public.games VALUES (37, 2018, 'Quarter-Final', 26, 29, 3, 2);
-INSERT INTO public.games VALUES (38, 2018, 'Quarter-Final', 28, 30, 2, 0);
-INSERT INTO public.games VALUES (39, 2018, 'Quarter-Final', 27, 31, 2, 1);
-INSERT INTO public.games VALUES (40, 2018, 'Quarter-Final', 25, 32, 2, 0);
-INSERT INTO public.games VALUES (41, 2018, 'Eighth-Final', 28, 33, 2, 1);
-INSERT INTO public.games VALUES (42, 2018, 'Eighth-Final', 30, 34, 1, 0);
-INSERT INTO public.games VALUES (43, 2018, 'Eighth-Final', 27, 35, 3, 2);
-INSERT INTO public.games VALUES (44, 2018, 'Eighth-Final', 31, 36, 2, 0);
-INSERT INTO public.games VALUES (45, 2018, 'Eighth-Final', 26, 37, 2, 1);
-INSERT INTO public.games VALUES (46, 2018, 'Eighth-Final', 29, 38, 2, 1);
-INSERT INTO public.games VALUES (47, 2018, 'Eighth-Final', 32, 39, 2, 1);
-INSERT INTO public.games VALUES (48, 2018, 'Eighth-Final', 25, 40, 4, 3);
-INSERT INTO public.games VALUES (49, 2014, 'Final', 41, 40, 1, 0);
-INSERT INTO public.games VALUES (50, 2014, 'Third Place', 42, 31, 3, 0);
-INSERT INTO public.games VALUES (51, 2014, 'Semi-Final', 40, 42, 1, 0);
-INSERT INTO public.games VALUES (52, 2014, 'Semi-Final', 41, 31, 7, 1);
-INSERT INTO public.games VALUES (53, 2014, 'Quarter-Final', 42, 43, 1, 0);
-INSERT INTO public.games VALUES (54, 2014, 'Quarter-Final', 40, 27, 1, 0);
-INSERT INTO public.games VALUES (55, 2014, 'Quarter-Final', 31, 33, 2, 1);
-INSERT INTO public.games VALUES (56, 2014, 'Quarter-Final', 41, 25, 1, 0);
-INSERT INTO public.games VALUES (57, 2014, 'Eighth-Final', 31, 44, 2, 1);
-INSERT INTO public.games VALUES (58, 2014, 'Eighth-Final', 33, 32, 2, 0);
-INSERT INTO public.games VALUES (59, 2014, 'Eighth-Final', 25, 45, 2, 0);
-INSERT INTO public.games VALUES (60, 2014, 'Eighth-Final', 41, 46, 2, 1);
-INSERT INTO public.games VALUES (61, 2014, 'Eighth-Final', 42, 36, 2, 1);
-INSERT INTO public.games VALUES (62, 2014, 'Eighth-Final', 43, 47, 2, 1);
-INSERT INTO public.games VALUES (63, 2014, 'Eighth-Final', 40, 34, 1, 0);
-INSERT INTO public.games VALUES (64, 2014, 'Eighth-Final', 27, 48, 2, 1);
+COPY public.games (game_id, year, round, winner_id, opponent_id, winner_goals, opponent_goals) FROM stdin;
+33	2018	Final	25	26	4	2
+34	2018	Third Place	27	28	2	0
+35	2018	Semi-Final	26	28	2	1
+36	2018	Semi-Final	25	27	1	0
+37	2018	Quarter-Final	26	29	3	2
+38	2018	Quarter-Final	28	30	2	0
+39	2018	Quarter-Final	27	31	2	1
+40	2018	Quarter-Final	25	32	2	0
+41	2018	Eighth-Final	28	33	2	1
+42	2018	Eighth-Final	30	34	1	0
+43	2018	Eighth-Final	27	35	3	2
+44	2018	Eighth-Final	31	36	2	0
+45	2018	Eighth-Final	26	37	2	1
+46	2018	Eighth-Final	29	38	2	1
+47	2018	Eighth-Final	32	39	2	1
+48	2018	Eighth-Final	25	40	4	3
+49	2014	Final	41	40	1	0
+50	2014	Third Place	42	31	3	0
+51	2014	Semi-Final	40	42	1	0
+52	2014	Semi-Final	41	31	7	1
+53	2014	Quarter-Final	42	43	1	0
+54	2014	Quarter-Final	40	27	1	0
+55	2014	Quarter-Final	31	33	2	1
+56	2014	Quarter-Final	41	25	1	0
+57	2014	Eighth-Final	31	44	2	1
+58	2014	Eighth-Final	33	32	2	0
+59	2014	Eighth-Final	25	45	2	0
+60	2014	Eighth-Final	41	46	2	1
+61	2014	Eighth-Final	42	36	2	1
+62	2014	Eighth-Final	43	47	2	1
+63	2014	Eighth-Final	40	34	1	0
+64	2014	Eighth-Final	27	48	2	1
+\.
 
 
 --
 -- Data for Name: teams; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.teams VALUES (25, 'France');
-INSERT INTO public.teams VALUES (26, 'Croatia');
-INSERT INTO public.teams VALUES (27, 'Belgium');
-INSERT INTO public.teams VALUES (28, 'England');
-INSERT INTO public.teams VALUES (29, 'Russia');
-INSERT INTO public.teams VALUES (30, 'Sweden');
-INSERT INTO public.teams VALUES (31, 'Brazil');
-INSERT INTO public.teams VALUES (32, 'Uruguay');
-INSERT INTO public.teams VALUES (33, 'Colombia');
-INSERT INTO public.teams VALUES (34, 'Switzerland');
-INSERT INTO public.teams VALUES (35, 'Japan');
-INSERT INTO public.teams VALUES (36, 'Mexico');
-INSERT INTO public.teams VALUES (37, 'Denmark');
-INSERT INTO public.teams VALUES (38, 'Spain');
-INSERT INTO public.teams VALUES (39, 'Portugal');
-INSERT INTO public.teams VALUES (40, 'Argentina');
-INSERT INTO public.teams VALUES (41, 'Germany');
-INSERT INTO public.teams VALUES (42, 'Netherlands');
-INSERT INTO public.teams VALUES (43, 'Costa Rica');
-INSERT INTO public.teams VALUES (44, 'Chile');
-INSERT INTO public.teams VALUES (45, 'Nigeria');
-INSERT INTO public.teams VALUES (46, 'Algeria');
-INSERT INTO public.teams VALUES (47, 'Greece');
-INSERT INTO public.teams VALUES (48, 'United States');
+COPY public.teams (team_id, name) FROM stdin;
+25	France
+26	Croatia
+27	Belgium
+28	England
+29	Russia
+30	Sweden
+31	Brazil
+32	Uruguay
+33	Colombia
+34	Switzerland
+35	Japan
+36	Mexico
+37	Denmark
+38	Spain
+39	Portugal
+40	Argentina
+41	Germany
+42	Netherlands
+43	Costa Rica
+44	Chile
+45	Nigeria
+46	Algeria
+47	Greece
+48	United States
+\.
 
 
 --
